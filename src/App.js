9c3fc39b0587a54fcc7d17db46826991f06191fe
Musratov <musratof@gmail.com>
2022-05-27 12:25:49 +0500

    ThemeProvider added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import './App.scss';
 import { Button, Typography } from '@mui/material';
 import { Add, Settings, } from '@mui/icons-material';
 import { margin } from '@mui/system';
+import styled from '@emotion/styled';
 
 function App() {
+
+  const BlueButton = styled(Button) (({theme})=>({
+    backgroundColor:theme.palette.otherColor.main,
+    color:"#888",
+    margin:5,
+    "&:hover":{
+      backgroundColor:"lightblue"
+    },
+    "&:disabled":{
+      backgroundColor:"gray",
+      color:"white",
+    }
+  }))
+  
 	return (
 		<div>
 			<Button size='small' variant='text'>
@@ -27,48 +42,8 @@ function App() {
 			<Typography variant='h1' component='h2'>
 				it uses h1 style  but it's a p tag
 			</Typography>
-      <Button variant='contained'
-      sx={{
-        backgroundColor:"skyblue",
-        color:"#888",
-        margin:5,
-        "&:hover":{
-          backgroundColor:"lightblue"
-        },
-        "&:disabled":{
-          backgroundColor:"gray",
-          color:"white"
-        }
-      }}
-      >My Unique Button</Button>
-      <Button variant='contained' 
-      sx={{
-        backgroundColor:"skyblue",
-        color:"#888",
-        margin:5,
-        "&:hover":{
-          backgroundColor:"lightblue"
-        },
-        "&:disabled":{
-          backgroundColor:"gray",
-          color:"white"
-        }
-      }}
-      >My Unique Button</Button>
-      <Button variant='contained'
-      sx={{
-        backgroundColor:"skyblue",
-        color:"#888",
-        margin:5,
-        "&:hover":{
-          backgroundColor:"lightblue"
-        },
-        "&:disabled":{
-          backgroundColor:"gray",
-          color:"white"
-        }
-      }}
-      >My Unique Button</Button>
+     <BlueButton>My Button</BlueButton>
+     <BlueButton>Another Button</BlueButton>
 		</div>
 	);
 }
