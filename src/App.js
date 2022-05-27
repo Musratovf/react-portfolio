ab5fa409b562a13219b2853092ddecd23465946b
Musratov <musratof@gmail.com>
2022-05-27 12:08:04 +0500

    Btns file added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
 import React from 'react';
-import "./App.scss"
+import './App.scss';
+import { Button, Typography } from '@mui/material';
+import { Add, Settings, } from '@mui/icons-material';
+import { margin } from '@mui/system';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
-};
+function App() {
+	return (
+		<div>
+			<Button size='small' variant='text'>
+				Text
+			</Button>
+			<Button
+				startIcon={<Settings />}
+				variant='contained'
+				color='secondary'
+				size='small'>
+				Settings
+			</Button>
+			<Button
+				startIcon={<Add />}
+				variant='contained'
+				color='success'
+				size='small'>
+				Add new post
+			</Button>
+			<Typography variant='h1' component='h2'>
+				it uses h1 style  but it's a p tag
+			</Typography>
+      <Button variant='contained'
+      sx={{
+        backgroundColor:"skyblue",
+        color:"#888",
+        margin:5,
+        "&:hover":{
+          backgroundColor:"lightblue"
+        },
+        "&:disabled":{
+          backgroundColor:"gray",
+          color:"white"
+        }
+      }}
+      >My Unique Button</Button>
+      <Button variant='contained' 
+      sx={{
+        backgroundColor:"skyblue",
+        color:"#888",
+        margin:5,
+        "&:hover":{
+          backgroundColor:"lightblue"
+        },
+        "&:disabled":{
+          backgroundColor:"gray",
+          color:"white"
+        }
+      }}
+      >My Unique Button</Button>
+      <Button variant='contained'
+      sx={{
+        backgroundColor:"skyblue",
+        color:"#888",
+        margin:5,
+        "&:hover":{
+          backgroundColor:"lightblue"
+        },
+        "&:disabled":{
+          backgroundColor:"gray",
+          color:"white"
+        }
+      }}
+      >My Unique Button</Button>
+		</div>
+	);
+}
 
 export default App;
