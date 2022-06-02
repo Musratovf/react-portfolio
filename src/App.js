2e546776633d6e3d834cf33c8ede0336d63b3375
Musratov <musratof@gmail.com>
2022-06-02 12:00:38 +0500

    MUI layout file added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.scss';
-// import Sidebar from './Components/Sidebar';
-// import Feed from './Components/Feed';
-// import Rightbar from './Components/Rightbar';
+import Sidebar from './Components/Sidebar';
+import Feed from './Components/Feed';
+import Rightbar from './Components/Rightbar';
 
 function App() {
 	return (
 		<div>
-			{/* <Sidebar />
+			<Sidebar />
 			<Feed />
-			<Rightbar /> */}
+			<Rightbar />
 		</div>
 	);
 }
