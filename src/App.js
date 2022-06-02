ff98a17641a4e7f3ff261ad6b0fe1f99d9536f0e
Musratov <musratof@gmail.com>
2022-06-02 11:59:50 +0500

    create theme MUI Themes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import React from 'react';
 import './App.scss';
-import { Button, Typography } from '@mui/material';
-import { Add, Settings, } from '@mui/icons-material';
-import styled from '@emotion/styled';
+// import Sidebar from './Components/Sidebar';
+// import Feed from './Components/Feed';
+// import Rightbar from './Components/Rightbar';
 
 function App() {
-  const BlueButton = styled(Button) ({
-    backgroundColor:"skyblue",
-    color:"#888",
-    margin:5,
-    "&:hover":{
-      backgroundColor:"lightblue"
-    },
-    "&:disabled":{
-      backgroundColor:"gray",
-      color:"white",
-    }
-  })
-  
 	return (
 		<div>
-			<Button size='small' variant='text'>
-				Text
-			</Button>
-			<Button
-				startIcon={<Settings />}
-				variant='contained'
-				color='secondary'
-				size='small'>
-				Settings
-			</Button>
-			<Button
-				startIcon={<Add />}
-				variant='contained'
-				color='success'
-				size='small'>
-				Add new post
-			</Button>
-			<Typography variant='h1' component='h2'>
-				it uses h1 style  but it's a p tag
-			</Typography>
-     <BlueButton>My Button</BlueButton>
-     <BlueButton>Another Button</BlueButton>
+			{/* <Sidebar />
+			<Feed />
+			<Rightbar /> */}
 		</div>
 	);
 }
