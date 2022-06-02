446dae83dc5147b92db22f437a8dcf7011937671
Musratov <musratof@gmail.com>
2022-06-02 12:25:08 +0500

    Mui layouts (Box, Container, Grid, Stack,)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.scss';
 import Sidebar from './Components/Sidebar';
 import Feed from './Components/Feed';
 import Rightbar from './Components/Rightbar';
+import { Box, Container, Stack } from '@mui/material';
 
 function App() {
 	return (
-		<div>
-			<Sidebar />
-			<Feed />
-			<Rightbar />
-		</div>
+		<Box>
+			{/* navbar */}
+			<Stack direction={'row'} spacing={2} justifyContent="space-between">
+				<Sidebar />
+				<Feed />
+				<Rightbar />
+			</Stack>
+		</Box>
 	);
 }
 
