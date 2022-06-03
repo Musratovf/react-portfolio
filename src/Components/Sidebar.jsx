352a43e70b9d84e4dbbf0945b23856b07716014d
Musratov <musratof@gmail.com>
2022-06-03 21:09:11 +0500

    sidebar icons added

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import React from 'react';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { Home } from '@mui/icons-material';
 
 const Sidebar = () => {
-  return (
-    <Box bgcolor="skyblue" flex={1} p={2} sx={{ display: { xs: 'none', sm: "block" }}}>Sidebar</Box>
-  )
-}
+	return (
+		<Box
+			bgcolor='skyblue'
+			flex={1}
+			p={2}
+			sx={{ display: { xs: 'none', sm: 'block' } }}>
+			
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#home'>
+              <ListItemIcon>
+                <Home/>
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          </List>
+		</Box>
+    
+	);
+};
 
-export default Sidebar
+export default Sidebar;
