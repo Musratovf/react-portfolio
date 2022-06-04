425615e50d28a74d4df7c540ebe3ab44c885c54c
Musratov <musratof@gmail.com>
2022-06-04 10:04:01 +0500

    material ui added icons

--- src/Components/Rightbar.jsx
+++ src/Components/Rightbar.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Avatar, AvatarGroup, Box, Typography } from '@mui/material';
 
 const Rightbar = () => {
 	return (
 		<>
 		<Box
-		bgcolor='lightcoral'
 		flex={2}
 		p={2}
 		sx={{ display: { xs: 'none', sm: 'block' } }}>
 		<Box position={"fixed"}>
-			Rightbar
+			<Typography variant='h6' fontWeight={100}>
+				Online Friends
+			</Typography>
+		<AvatarGroup max={4}>
+  			<Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+  			<Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/2.jpg" />
+  			<Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/3.jpg" />
+  			<Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/4.jpg" />
+  			<Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/5.jpg" />
+		</AvatarGroup>
 		</Box>
 		</Box>
 		</>
