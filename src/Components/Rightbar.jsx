b93e2eff84b2c6c695c3c5acb3310cf862e81753
Musratov <musratof@gmail.com>
2022-06-07 10:35:46 +0500

    added tooltip rightbar section

--- src/Components/Rightbar.jsx
+++ src/Components/Rightbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Avatar, AvatarGroup, Box, ImageList, ImageListItem, Typography } from '@mui/material';
+import {
+	Avatar,
+	AvatarGroup,
+	Box,
+	Divider,
+	ImageList,
+	ImageListItem,
+	List,
+	ListItem,
+	ListItemAvatar,
+	ListItemText,
+	Typography,
+} from '@mui/material';
 
 const Rightbar = () => {
 	return (
 		<>
 			<Box flex={2} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
 				<Box position={'fixed'} width={300}>
-				<Typography variant='h6' fontWeight={100}>
+					<Typography variant='h6' fontWeight={100}>
 						Online Friends
 					</Typography>
 					<AvatarGroup max={4}>
@@ -35,28 +47,112 @@ const Rightbar = () => {
 						Lattest Photos
 					</Typography>
 					<ImageList cols={3} rowHeight={100}>
-						<ImageListItem >
+						<ImageListItem>
 							<img
-								src="https://material-ui.com/static/images/image-list/breakfast.jpg"
-								alt="Image list img"
+								src='https://material-ui.com/static/images/image-list/breakfast.jpg'
+								alt='Image list img'
 							/>
 						</ImageListItem>
-						<ImageListItem >
+						<ImageListItem>
 							<img
-								src="https://material-ui.com/static/images/image-list/breakfast.jpg"
-								alt="Image list img"
+								src='https://material-ui.com/static/images/image-list/breakfast.jpg'
+								alt='Image list img'
 							/>
 						</ImageListItem>
-						<ImageListItem >
+						<ImageListItem>
 							<img
-								src="https://material-ui.com/static/images/image-list/burgers.jpg"
-								alt="Image list img"
+								src='https://material-ui.com/static/images/image-list/burgers.jpg'
+								alt='Image list img'
 							/>
 						</ImageListItem>
 					</ImageList>
-					<Typography variant='h6' fontWeight={100} mt={2} mb={2}>
+					<Typography variant='h6' fontWeight={100} mt={2}>
 						Latest Convetions
 					</Typography>
+					<List
+						sx={{
+							width: '100%',
+							maxWidth: 360,
+							bgcolor: 'background.paper',
+						}}>
+						<ListItem alignItems='flex-start'>
+							<ListItemAvatar>
+								<Avatar
+									alt='Remy Sharp'
+									src='/static/images/avatar/1.jpg'
+								/>
+							</ListItemAvatar>
+							<ListItemText
+								primary='Brunch this weekend?'
+								secondary={
+									<React.Fragment>
+										<Typography
+											sx={{ display: 'inline' }}
+											component='span'
+											variant='body2'
+											color='text.primary'>
+											Ali Connors
+										</Typography>
+										{
+											" — I'll be in your neighborhood doing errands this…"
+										}
+									</React.Fragment>
+								}
+							/>
+						</ListItem>
+						<Divider variant='inset' component='li' />
+						<ListItem alignItems='flex-start'>
+							<ListItemAvatar>
+								<Avatar
+									alt='Travis Howard'
+									src='/static/images/avatar/2.jpg'
+								/>
+							</ListItemAvatar>
+							<ListItemText
+								primary='Summer BBQ'
+								secondary={
+									<React.Fragment>
+										<Typography
+											sx={{ display: 'inline' }}
+											component='span'
+											variant='body2'
+											color='text.primary'>
+											to Scott, Alex, Jennifer
+										</Typography>
+										{
+											" — Wish I could come, but I'm out of town this…"
+										}
+									</React.Fragment>
+								}
+							/>
+						</ListItem>
+						<Divider variant='inset' component='li' />
+						<ListItem alignItems='flex-start'>
+							<ListItemAvatar>
+								<Avatar
+									alt='Cindy Baker'
+									src='/static/images/avatar/3.jpg'
+								/>
+							</ListItemAvatar>
+							<ListItemText
+								primary='Oui Oui'
+								secondary={
+									<React.Fragment>
+										<Typography
+											sx={{ display: 'inline' }}
+											component='span'
+											variant='body2'
+											color='text.primary'>
+											Sandra Adams
+										</Typography>
+										{
+											' — Do you have Paris recommendations? Have you ever…'
+										}
+									</React.Fragment>
+								}
+							/>
+						</ListItem>
+					</List>
 				</Box>
 			</Box>
 		</>
