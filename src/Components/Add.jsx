c902c0117c91bdd96a95a49dab0aef65da459441
Musratov <musratof@gmail.com>
2022-06-07 22:13:54 +0500

    TextField added Mui

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -1,22 +1,28 @@
-import { Fab, Modal, styled, Tooltip, Typography } from '@mui/material';
+import { Avatar, Fab, Modal, styled, TextField, Tooltip, Typography } from '@mui/material';
 import { React, useState } from 'react';
 import { Add as AddIcon } from '@mui/icons-material';
 import { Box } from '@mui/system';
 
 const Add = () => {
+	const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false );
-    
-    const StyledModal = styled(Modal)({
-        display:'flex',
-        alignItems:"center",
-        justifyContent:"center"
-    })
-    
+	const StyledModal = styled(Modal)({
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	});
+
+	const UserBox = styled(Box)({
+		display: 'flex',
+		alignItems: 'center',
+		gap: '20px',
+		marginBottom: '20px',
+	});
+
 	return (
 		<>
 			<Tooltip
-                onClick={e=>setOpen(true)}
+				onClick={(e) => setOpen(true)}
 				title='Delete'
 				sx={{
 					position: 'fixed',
@@ -29,12 +35,30 @@ const Add = () => {
 			</Tooltip>
 			<StyledModal
 				open={open}
-				onClose={e=>setOpen(false)}
+				onClose={(e) => setOpen(false)}
 				aria-labelledby='modal-modal-title'
 				aria-describedby='modal-modal-description'>
-				<Box width={400} height={100} bgcolor="white" p={2} >
-                    <Typography variant='h6' color="gray" textAlign="center">Create new Post</Typography>
-                    
+				<Box width={400} height={200} bgcolor='white' p={2}>
+					<Typography variant='h6' color='gray' textAlign='center'>
+						Create new Post
+					</Typography>
+					<UserBox>
+						<Avatar
+							src='https://material-ui.com/static/images/avatar/1.jpg'
+							sx={{ width: 30, height: 30 }}
+						/>
+						<Typography fontWeight={500} variant='span'>
+							Farrukh dev
+						</Typography>
+					</UserBox>
+					<TextField
+						id='standard-multiline-static'
+						label='Multiline'
+						multiline
+						rows={4}
+						defaultValue='Default Value'
+						variant='standard'
+					/>
 				</Box>
 			</StyledModal>
 		</>
