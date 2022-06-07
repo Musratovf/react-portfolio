8ff908c505441e70cb315eed9be9feed46658d98
Musratov <musratof@gmail.com>
2022-06-07 19:55:44 +0500

    Mui modal added

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -1,15 +1,35 @@
-import { Fab, Tooltip } from '@mui/material';
-import React from 'react';
-import { Add as AddIcon } from "@mui/icons-material"
+import { Fab, Modal, Tooltip } from '@mui/material';
+import { React, useState } from 'react';
+import { Add as AddIcon } from '@mui/icons-material';
+import { Box } from '@mui/system';
 
 const Add = () => {
+
+    const [open, setOpen] = useState(false );
+    
 	return (
 		<>
-			<Tooltip title='Delete' sx={{ position:"fixed", bottom:20 }}>
+			<Tooltip
+                onClick={e=>setOpen(true)}
+				title='Delete'
+				sx={{
+					position: 'fixed',
+					bottom: 20,
+					left: { xs: 'calc(50% - 25px)', md: 30 },
+				}}>
 				<Fab color='primary' aria-label='add'>
 					<AddIcon />
 				</Fab>
 			</Tooltip>
+			<Modal
+				open={open}
+				onClose={e=>setOpen(false)}
+				aria-labelledby='modal-modal-title'
+				aria-describedby='modal-modal-description'>
+				<Box>
+					Hello
+				</Box>
+			</Modal>
 		</>
 	);
 };
